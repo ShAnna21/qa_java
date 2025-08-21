7e81f02d8b636c3c8eecfec2187a8a7e18e4378a
Anna Shkireva <anna.shkireva88@yandex.ru>
2025-08-21 12:38:35 +0400

    fix Lion class and writeTest

---
+++ src/test/java/com/example/LionParametrizedTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+
+public class LionParametrizedTest {
+
+    private Feline feline;
+
+    @BeforeEach
+    public void setUp() {
+        feline = Mockito.mock(Feline.class);
+    }
+    @ParameterizedTest
+    @MethodSource ("data")
+    public void LionParameterizedTest(String sex, boolean hasMane) throws Exception {
+        Lion lion = new Lion(feline, sex);
+        boolean expected = hasMane;
+        boolean actual = lion.doesHaveMane();
+        Assertions.assertEquals(expected, actual);
+
+    }
+    public static Object[][] data() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+}
